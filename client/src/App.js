c49f60f57f366183bdb5051818907b31f40b6022
Jessica Lam <jemlam@jessicas-mbp.lan>
2020-02-23 18:23:54 -0800

    created login, protected route, axiosWithAuth and created get request for BubblePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Switch>
         <Route exact path="/" component={Login} />
+        <ProtectedRoute exact path="/protected" component={BubblePage} />
+        </Switch>
+
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
